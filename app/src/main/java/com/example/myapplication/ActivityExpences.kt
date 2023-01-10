184b9c6bbcd3f148294d5963d0a6daf9bcea74cd
trss.trru@gmail.com <trss.trru@gmail.com>
2023-01-10 14:18:27 +0900

    Поменял dp на sp у текста. Добавил один слушатель на все кнопки в Расходах

--- app/src/main/java/com/example/myapplication/ActivityExpences.kt
+++ app/src/main/java/com/example/myapplication/ActivityExpences.kt
@@ -1,12 +1,30 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityExpencesBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class ActivityExpences : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityExpencesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expences)
+        bindingClass = ActivityExpencesBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
     }
+
+    fun onClickExpencesButon(view : View){
+        val testid = view.toString()
+        bindingClass.textExpenses11.text = "test"
+
+    }
+
+
+
 
 }
